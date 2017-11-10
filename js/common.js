84ea03148de87a3459986077e0c6bf29e1b11b7c
Marta Zhmikhova <marta.zhmikhova@gmail.com>
2017-11-10 12:41:46 +0200

    add

--- js/common.js
+++ js/common.js
@@ -26,12 +26,7 @@
      });
 
  	//scrol
-     $("ul").on("click","a", function (event) {
-         event.preventDefault();
-         var id  = $(this).attr('href'),
-             top = $(id).offset().top;
-         $('body,html').animate({scrollTop: top}, 2000);
-     });
+     
      $(".scrol").click(function(event){
          event.preventDefault();
          var id  = $(this).attr('href'),
